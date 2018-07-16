1b5c5a8330270d7c4256bc9b77fd1cb501baedb6
Jacek Podkanski <jacek.podkanski@conceptlifesciences.com>
2018-07-16 14:05:18 +0100

    more debugging

--- lib/roo/open_office.rb
+++ lib/roo/open_office.rb
@@ -444,6 +444,9 @@ module Roo
       key = [y, x + i]
       @cell_type[sheet] ||= {}
       @cell_type[sheet][key] = value_type.to_sym if value_type
+      byebug
+      9==9
+
       @cell_spanning[sheet][key] = spanning if spanning
       @formula[sheet] ||= {}
       if formula
